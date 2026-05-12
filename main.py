edd53b3c0bb1a58f66104f62f718f375084c30b9
Lkings2D <pixelkingmmo@gmail.com>
2026-05-11 23:27:36 -0400

    debug testing

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
+import requests
 from fastapi import FastAPI
+
+print("🟢 FILE STARTING")
 
 app = FastAPI()
 
+print("🟢 FASTAPI CREATED")
+
 @app.get("/")
 def root():
+    print("📡 ROOT HIT")
     return {"status": "alive"}
