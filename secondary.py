b1e426567ada6339c3ee3d327c80aa98cd5cb117
Lkings2D <pixelkingmmo@gmail.com>
2026-07-10 13:06:22 -0400

    - updated list

--- secondary.py
+++ secondary.py
@@ -32,7 +32,7 @@ PASSWORD = os.getenv("DASH_PASSWORD")
 UUID = os.getenv("SECRET_UUID")
 
 # Supported stocks for alerts <------------------------------------------------
-SUPPORTED_STOCKS = ["META"]
+SUPPORTED_STOCKS = ["META","VRT","LITE",]
 
 YAHOO_CHART_URL = "https://query1.finance.yahoo.com/v8/finance/chart/{symbol}"
 EASTERN = ZoneInfo("America/New_York")
